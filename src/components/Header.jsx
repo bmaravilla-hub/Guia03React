ae1830e7e42216d1ed25c26dce7c8e4858830eba
bmaravilla-hub <maravillacruz806@gmail.com>
2025-02-08 11:05:30 -0600

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+export const Header = ({ 
+    allProducts, 
+    setAllProducts, 
+    total, 
+    setTotal, 
+    countProducts, 
+    setCountProducts 
+}) => {
+    const [active, setActive] = React.useState(false);
+    
+    const onDeleteProduct = product => {
+    const results = allProducts.filter(item => item.id !== product.id);
+    setTotal(total - product.price * product.quantity);
+    setCountProducts(countProducts - product.quantity);
+    setAllProducts(results);
+  }
+
+  const onClearCart = () => {
+    setAllProducts([]);
+    setTotal(0);
+    setCountProducts(0);
+  };
+
+  return (
+    <header>
+      <h1>Tienda de Libros</h1>
+      <div className="container-icon">
+        <div className="container-cart-icon" 
+        onClick={() => setActive(!active)}
+        >
+            <img 
+            src="https://e7.pngegg.com/pngimages/833/426/png-clipart-shopping-cart-icon-shopping-cart-black-design.png" 
+            alt="carrito" 
+            className="icon-cart" 
+            />
+          <div className="count-products">
+            <span id="contador-productos">{countProducts}</span>
+          </div>
+        </div>
+        <div className={`container-cart-products ${active ? '' : 'hidden-cart'}`}>
+          {allProducts.length ? (
+            <>
+              <div className="row-product">
+                {allProducts.map(product => (
+                  <div className="cart-product" key={product.id}>
+                    <div className="info-cart-product">
+                      <span className="cantidad-producto-carrito">{product.quantity}</span>
+                      <p className="titulo-producto-carrito">{product.title}</p>
+                      <span className="precio-producto-carrito">${product.price}</span>
+                    </div>
+                    <img src="https://static.vecteezy.com/system/resources/previews/024/696/736/non_2x/red-close-icon-free-png.png" 
+                    alt="cerrar" 
+                    className="icon-close" 
+                    onClick={() => onDeleteProduct(product)} 
+                    />
+                  </div>
+                ))}
+             
+              <div className="cart-total">
+                <h3>Total:</h3>
+                <span className="total-pagar">${total}</span>
+              </div>
+              <button className="btn-clear-all" onClick={onClearCart}>Vaciar Carrito</button>
+             </div>
+             </>
+          ) : (
+            <p className="cart-empty">El carrito está vacío</p>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Cien años de soledad',
+        price: 100,
+        urlImage: 'https://prodimage.images-bn.com/pimages/9798890981745_p1_v2_s1200x1200.jpg',
+    quantity: 1
+    },
+    {
+        id: 2,
+        title: 'El señor de los anillos(Trilogía)',
+        price: 190,
+        urlImage: 'https://www.planetadelibros.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+    },
+{
+    id: 3,
+        title: 'Cuentos de Barro',
+            price: 30,
+                urlImage: 'https://m.media-amazon.com/images/I/41dZy8CP52L._SY445_SX342_.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+        title: 'Tierra de Infancia',
+            price: 30,
+                urlImage: 'https://leolibri.net/image/cache/catalog/2024/febrero%2026%20ev/Lars-TierradeInfancia-927x1000.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+        title: 'Harry Potter Pack',
+            price: 390,
+                urlImage: 'https://i5.walmartimages.com/seo/HARRY-POTTER-Pack-HarryPotter-La-serie-completa-Harry-Potter-Paperback-Boxed-Set-Books-1-7-Paperback-9788418173196_983ae67f-131d-4378-ba10-3ceadef50dde.4d4a65cd092771e4c042cb948e26be06.jpeg',
+    quantity: 1
+}
+       ];
